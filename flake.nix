4f83a51e3cdaf4896795aa4dc033a8b7fdaa03c4
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-03-15 18:52:38 +0100

    Consume submodule. update inputs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix = {
-      url = "path:/home/roland/my_poetry2nix";
+      url = "path:/home/roland/poetry2nix-testing/poetry2nix";
       # url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
