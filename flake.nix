0270bfc198f136d744fc2e730b4a005c19044d0f
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-05-04 21:24:51 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     poetry2nix = {
       url = "path:/home/roland/poetry2nix-testing/poetry2nix";
       # url = "github:nix-community/poetry2nix";
